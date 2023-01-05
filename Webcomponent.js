b3652d5ae73bb285ee0c162ab3231cda26361b99
SumanHiremath113 <121942693+SumanHiremath113@users.noreply.github.com>
2023-01-05 14:39:20 +0530

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -21,8 +21,8 @@
         }
 
         fireChange() {
-            console.log("OnClick Triggered");     
-            document.getElementById().innerHTML = "Suman";   
+            //console.log("OnClick Triggered");     
+            document.write = "Suman";   
            
         }        
         
